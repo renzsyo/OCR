fd62ef2ba77a801ff1dbd486f3b3ea9a50cbcdc6
twinkleonez <danielbon784@gmail.com>
2026-03-19 16:30:59 +0800

    Delete OCR-main directory

--- OCR-main/IDscanner/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cam_handler import CamHandler
-from .file_handler import FileManager
-from .inference_handler import InferenceHandler
-from .review_handler import ReviewHandler
-from .load_ui import UiLoader
